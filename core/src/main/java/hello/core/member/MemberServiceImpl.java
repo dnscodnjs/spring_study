d18e02610e851927aee141bc5ace80a7184f0086
dnscodnjs <yun9869@naver.com>
2023-08-01 18:34:05 +0900

    회원 도메인 실행과 테스트

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,9 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    //MemberServiceImpl 은 추상화와 구현체 둘다 의존하고 있어서 DIP 위반.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
 
     @Override
     public void join(Member member) {
