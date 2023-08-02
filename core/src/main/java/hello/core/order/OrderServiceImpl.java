0a09fc6ae60c4ea1423ef9259a223c2024e2e5ed
dnscodnjs <yun9869@naver.com>
2023-08-02 15:49:53 +0900

    새로운 할인 정책 적용과 문제점

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -13,8 +14,15 @@ public class OrderServiceImpl implements OrderService{
     //회원 정보 조회 후
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     
-    //할인 정책에 회원 넘기기
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //할인 정책에 회원 넘기기, 이 코드는 추상(인터페이스)와 구체 클래스에 모두 의존하고 있어서 DIP 위반.
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    //위 고정 할인 시스템에서 비율 할인으로 바뀌면서 OrderServiceImple의 소스코드도 변경되었으므로 OCP 위반.
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //구현체를 없애고 인터페이스만 연결 ( DIP 위반 x ), but 연결된게 없어서 nullpointexception이 뜬다
+    private DiscountPolicy discountPolicy;
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
