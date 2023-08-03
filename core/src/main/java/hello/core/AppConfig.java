805e649d9479abd48d5e3b561b696064199cc4b2
dnscodnjs <yun9869@naver.com>
2023-08-03 12:51:02 +0900

    컨테이너에 등록된 모든 빈 조회

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,7 @@ public class AppConfig {
 
     //각 메서드에 @Bean을 적어주면 스프링 컨테이너에 등록된다.
     //@Bean이 붙은 메서드의 명을 스프링 빈의 이름으로 사용한다. ex) memberService, orderService
+    //빈의 이름은 항상 다르게 부여
     @Bean
     public MemberService memberService() {
         //생성자를 통해 생성한 객체(new 인스턴스 생성된것)이 들어간 것 : 용어로 생성자 주입이라고 한다.
