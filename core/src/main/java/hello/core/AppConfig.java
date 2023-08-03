82c7139950ddf59ad7dcb4016be790e130fb7740
dnscodnjs <yun9869@naver.com>
2023-08-03 12:11:30 +0900

    스프링으로 전환(스프링 컨테이너, 빈 사용)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -7,23 +7,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //설정 정보(구성 정보)
 public class AppConfig {
 
+    //각 메서드에 @Bean을 적어주면 스프링 컨테이너에 등록된다.
+    //@Bean이 붙은 메서드의 명을 스프링 빈의 이름으로 사용한다. ex) memberService, orderService
+    @Bean
     public MemberService memberService() {
         //생성자를 통해 생성한 객체(new 인스턴스 생성된것)이 들어간 것 : 용어로 생성자 주입이라고 한다.
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
 }
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,13 +7,22 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
         
-        AppConfig appConfig = new AppConfig();
+/*        AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService();*/
+
+        //ApplicationContext 를 스프링 컨테이너라고 한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //스프링 빈은 apllicationContext.getBean() 메서드를 사용해 찾을 수 있다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberId", Grade.Vip);
